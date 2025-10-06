444af4af28a0fb76d3e6f6a097033c6cb8d10dd1
Michael Maydew <michael.maydew@bouvet.no>
2025-10-06 14:34:09 +0200

    Basic DTL plus functionality

--- main.rs
+++ main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("DTL parser written in rust!");
+    let single = plus(10, vec![10]);
+    read_plus_return(single);
+
+    let multiple = plus(10, vec![1, 2, 3]);
+    read_plus_return(multiple);
+}
+
+fn read_plus_return(event: PlusReturn) {
+    match event {
+        PlusReturn::Num(x) => println!("Single: {}", x),
+        PlusReturn::NumArray(x) => println!("Multi: {:?}", x),
+    }
+}
+
+#[derive(Debug)]
+enum PlusReturn {
+    Num(i32),
+    NumArray(Vec<i32>),
+}
+
+fn plus(increment: i32, values: Vec<i32>) -> PlusReturn {
+    println!("\ninc: {}\nval: {:?}", increment, values);
+    let mut result = vec![];
+    for v in values {
+        result.push(increment + v);
+    }
+
+    if result.len() == 1 {
+        PlusReturn::Num(result[0])
+    } else {
+        PlusReturn::NumArray(result)
+    }
 }
